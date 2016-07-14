43e1d3c5e9651cf3978a9c5a4c00254ca0f30bdf
Jo-Jo Feng <yanlin.feng@yale.edu>
2016-07-14 14:49:12 -0400

    debuggint

--- splash/views.py
+++ splash/views.py
@@ -16,17 +16,17 @@ def hello(request):
 		body = request.POST.get('Body', None)
 		number = request.POST.get('From', None)
 
-		api.send_sms(body='The message you sent: ' + body, from_phone='+13098086245', to=['+1' + number])
+		# api.send_sms(body='The message you sent: ' + body, from_phone='+13098086245', to=['+1' + number])
 		
-		# twilio repsonse
-		# r = Response() # makes messages object
+		# twilio response
+		r = Response() # makes messages object
 
-		# if body is not None:
-		# 	r.message('The message you sent: ' + body)
-		# else:
-		# 	r.message('you didn\'t send anything...')
+		if body is not None:
+			r.message('The message you sent: ' + body + "; Your digits: " + number)
+		else:
+			r.message('Error')
 
-		# return HttpResponse(r.toxml(), content_type='text/xml')
+		return HttpResponse(r.toxml(), content_type='text/xml')
 
 	# if accessing the webpage via GET
 	elif request.method == 'GET':
