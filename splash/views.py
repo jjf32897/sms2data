c974baf707eff87337d2b74296af6e557edc5e36
Jo-Jo Feng <yanlin.feng@yale.edu>
2016-07-14 12:26:09 -0400

    testing twiml response

--- splash/views.py
+++ splash/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import twilio.twiml
 
 # Create your views here.
 def index(request):
 	return HttpResponse("placeholder ;)")
+
+def hello(request):
+    resp = twilio.twiml.Response()
+    resp.message("what's up hello")
+    return HttpResponse(str(resp))
